e66505ef6c1d78cf5c914a97558a60be8919eba0
King Khan <nsikan.na@gmail.com>
2022-01-30 19:36:36 -0500

    fiinished css for all sects except CTA Ad and Footer

--- src/components/Investment/Investment.js
+++ src/components/Investment/Investment.js
@@ -5,12 +5,12 @@ import Results from './Results'
 import Ad from './Ad'
 import CTA from './CTA'
 import Footer from './Footer'
+import Header from './Header'
 
 export default function Investment() {
   return <div>
       <NavBar/>
-      <h1>Investment Calculator</h1>
-      <p>Put your current investment totals, your contributions and your annual rate of return into the investment calculator, and we'll show you how much you can expect to have in your investment portfolio over time.</p>
+      <Header/>
       <Form/>
       <Results />
       <Ad/>
--- src/components/Investment/Results.js
+++ src/components/Investment/Results.js
@@ -3,68 +3,119 @@ import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 
 export default function Results() {
   return (
-    <div>
-      <h2>Your Results</h2>
-      <h3> ESTIMATED RETIREMENT SAVINGS</h3>
-      <p>In 19 years, your investment could be worth:</p>
-      <div>
-        <span>
-          <AttachMoneyIcon />
-        </span>
-        1,000,000
+    <div className="text-center ">
+      <div className="w-screen bg-gray-50 pb-10">
+        <div className=" mx-auto bg-white shadow-md w-10/12">
+          <div className="mx-auto w-8/12 ">
+            <h2 className="font-bold text-4xl pt-10 pb-6">Your Results</h2>
+            <div className="text-gray-500 font-bold text-xl w-10/12">
+              {" "}
+              ESTIMATED RETIREMENT SAVINGS
+            </div>
+            <p className="w-6/12 font-semibold text-lg">
+              In 19 years, your investment could be worth:
+            </p>
+            <div className="py-0.5 bg-gray-50 my-5"></div>
+            <div className="text-center">
+              <div className="text-lime-400 font-semibold pb-8">
+                <span>
+                  <AttachMoneyIcon className=" -translate-y-4 translate-x-1" />
+                </span>
+                <span className="text-4xl">1,000,000</span>
+              </div>
+              <img src="/images/landscape.jpg" className=" mx-auto mb-4" />
+              <div className="font-bold text-xl text-center mb-7">
+                Want to make a plan to meet your investment goals?
+              </div>
+              <button className="bg-sky-600 rounded-md text-white px-4 py-2 text-lg font-bold mb-6">
+                Work with a Pro
+              </button>
+              <img src="/images/money.jpg" className=" mx-auto mb-4" />
+              <div className="font-bold text-xl text-center mb-7">
+                Are you saving enough to retire the way you want?
+              </div>
+              <button className="bg-sky-600 rounded-md text-white px-4 py-2 text-lg font-bold mb-6">
+                Find Out
+              </button>
+              <img src="/images/list.jpg" className=" mx-auto mb-4" />
+
+              <div className="font-bold text-xl text-center mb-7">
+                Is money stress stopping you from investing more?
+              </div>
+              <button className="bg-sky-600 rounded-md text-white px-4 py-2 text-lg font-bold mb-6">
+                Take Control
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div>Want to make a plan to meet your investment goals?</div>
-      <button>Work with a Pro</button>
-      <div>Are you saving enough to retire the way you want?</div>
-      <button>Find</button>
-      <div>Is money stress stopping you from investing more?</div>
-      <button>Take Control</button>
-      <div>
+      <div className="text-center shadow pb-12">
         <div>
-          <div>INITIAL BALANCE</div>
-          <AttachMoneyIcon />
-          <div>4,000</div>
-          <div>0% of Total</div>
+          <div className="font-bold text-lg text-gray-500 ">
+            INITIAL BALANCE
+          </div>
+          <AttachMoneyIcon className="inline scale-75 -translate-y-3 translate-x-1" />
+          <div className="font-bold text-3xl inline">4,000</div>
+          <div className="text-2xl">0% of Total</div>
         </div>
         <div>
-          <div>CONTRIBUTIONS</div>
-          <AttachMoneyIcon />
-          <div>342,000</div>
-          <div>33% of Total</div>
+          <div className="font-bold text-lg text-gray-500 mt-7">
+            CONTRIBUTIONS
+          </div>
+          <AttachMoneyIcon className="inline scale-75 -translate-y-3 translate-x-1" />
+          <div className="font-bold text-3xl inline">342,000</div>
+          <div className="text-2xl">33% of Total</div>
         </div>
         <div>
-          <div>GROWTH</div>
-          <AttachMoneyIcon />
-          <div>694,557</div>
-          <div>67% of Total</div>
+          <div className="font-bold text-lg text-gray-500 mt-7">GROWTH</div>
+          <AttachMoneyIcon className="inline scale-75 -translate-y-3 translate-x-1" />
+          <div className="font-bold text-3xl inline">694,557</div>
+          <div className="text-2xl">67% of Total</div>
+        </div>
+        <div className="mt-16 ">
+          <img src="images/chartplaceholder.jpg" />
         </div>
       </div>
-      <div>
-        <h2>What if I...</h2>
-        <div>
-          <div>SAVED AN EXTRA $100 PER MONTH.</div>
-          <p>Adds $100 a month in contributions, but creates</p>
-          <div>
-            <AttachMoneyIcon />
-            44,801
+
+      <div className='w-9/12 mx-auto pb-10'>
+        <h2 className="font-bold text-4xl my-7">What if I...</h2>
+        <div className="pb-5">
+          <img src="/images/money2.jpg" className=" mx-auto mb-4" />
+          <div className="font-bold text-xl text-gray-700 mb-2">
+            SAVED AN EXTRA $100 PER MONTH.
+          </div>
+          <p className="text-xl text-gray-700 mb-2">
+            Adds $100 a month in contributions, but creates
+          </p>
+          <div className="text-lime-400 font-semibold ">
+            <AttachMoneyIcon className="inline scale-75 -translate-y-3 translate-x-1" />
+            <span className="font-bold text-3xl inline">44,801</span>
+          </div>
+          <p>in additional growth</p>
+        </div>
+        <div className="pb-5">
+          <img src="/images/coffee.jpg" className=" mx-auto mb-4" />
+          <div className="font-bold text-xl text-gray-700 mb-2">
+            GAVE UP DAILY COFFEE PURCHASES.
+          </div>
+          <p className="text-xl text-gray-700 mb-2">
+            Adds $128 a month in contributions, but creates
+          </p>
+          <div className="text-lime-400 font-semibold ">
+            <AttachMoneyIcon className="inline scale-75 -translate-y-3 translate-x-3"/>
+            <span className="font-bold text-3xl inline"> 57,345</span>
           </div>
           <p>in additional growth</p>
         </div>
         <div>
-          <div>GAVE UP DAILY COFFEE PURCHASES.</div>
-          <p>Adds $128 a month in contributions, but creates</p>
-          <div>
-            <AttachMoneyIcon />
-            57,345
-          </div>
-          <p>in additional growth</p>
-        </div>
-        <div>
-          <div>GAVE UP WEEKLY RESTAURANT VISITS.</div>
-          <p>Adds $200 a month in contributions, but creates</p>
-          <div>
-            <AttachMoneyIcon />
-            89,603
+          <img src="/images/plate.jpg" className=" mx-auto mb-4" />
+          <div className="font-bold text-xl text-gray-700 mb-2">GAVE UP WEEKLY RESTAURANT VISITS.</div>
+          <p className="text-xl text-gray-700 mb-2">
+            Adds $200 a month in contributions, but creates
+          </p>
+          <div className="text-lime-400 font-semibold ">
+            <AttachMoneyIcon className="inline scale-75 -translate-y-3 translate-x-3"/>
+            <span className="font-bold text-3xl inline"> 89,603</span>
           </div>
           <p>in additional growth</p>
         </div>
