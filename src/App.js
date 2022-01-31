8b4e0f49a461cdf08b5ed41117ca5260ebc1dec1
King Khan <nsikan.na@gmail.com>
2022-01-30 22:38:41 -0500

    moved investment tracker to new repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React from "react";
-import Wiki from "./components/Wiki";
-import Investment from "./components/Investment/Investment"
-
-import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
+import Wiki from "./Wiki";
 export default function App() {
   return (
-      <BrowserRouter className="flex">
-        <nav className="space-x-5 text-2xl font-semibold m-3 right-0 top-0 absolute ">
-          {/* <Link to="/wiki" className="hover:text-gray-400 text-4xl md:text-5xl xl:text-6xl">
-            1
-          </Link> */}
-
-        </nav>
-        <Routes>
-          <Route path="/" element={<Wiki />} />
-          <Route path="/wiki" element={<Wiki />} />
-          <Route path="/investment" element={<Investment />} />
-        </Routes>
-      </BrowserRouter>
-
+    <div >
+      <Wiki />
+    </div>
   );
 }
